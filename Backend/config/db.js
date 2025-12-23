cc73cf704f112978379082194ed9d13d52cfaa21
jayparmar2787@gmail.com <jayparmar2787@gmail.com>
2025-12-24 00:47:58 +0530

    remove .env file

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const DB_URL="mongodb+srv://jayparmar2787_db_user:iTrU4IajUF2rme1d@cluster0.kfxhcjp.mongodb.net/FixZoon";
 const ConnectDB=async()=>{
     try {
-        await mongoose.connect(DB_URL);
+        await mongoose.connect(process.env.DB_URL);
         console.log("DB Connected Succesfully");
     } catch (error) {
         console.log("DB Not Connected")
